208a3bb80d898a1f26c066531f3c40bb330fb182
R4hzafr <azharroisul05@gmail.com>
2026-05-25 12:19:32 +0700

    Fix:

--- Benchmark.java
+++ Benchmark.java
@@ -1,29 +1,19 @@
-import java.util.ArrayList;
-import java.util.Random;
-
 public class Benchmark {
     
-    // Menggenerasi kata acak untuk keperluan simulasi dataset kamus
-    public static ArrayList<String> generateWords(int count) {
-        ArrayList<String> words = new ArrayList<>();
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        Random rand = new Random();
-        
+    // Menggenerasi kata menggunakan Array bawaan (tanpa library)
+    public static String[] generateWords(int count) {
+        String[] words = new String[count]; // Memesan memori array sesuai jumlah data
         for (int i = 0; i < count; i++) {
-            StringBuilder sb = new StringBuilder();
-            int length = rand.nextInt(5) + 3; // Panjang kata acak antara 3-8 karakter
-            for (int j = 0; j < length; j++) {
-                sb.append(alphabet.charAt(rand.nextInt(alphabet.length())));
-            }
-            words.add(sb.toString());
+            // Membuat kata dummy, contoh: "kata0", "kata1", dst.
+            words[i] = "kata" + i;
         }
         return words;
     }
 
-    // 1. Simulasi Linear Search (Representasi Array/List biasa milik Orang 3)
-    public static boolean linearSearch(ArrayList<String> words, String target) {
-        for (String word : words) {
-            if (word.equals(target)) {
+    // 1. Simulasi Linear Search menggunakan iterasi Array standar
+    public static boolean linearSearch(String[] words, String target) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(target)) {
                 return true;
             }
         }
@@ -35,15 +25,15 @@ public class Benchmark {
     public static boolean suffixStructureSearch(String target) {
         int m = target.length();
         for (int i = 0; i < m; i++) {
-            char c = target.charAt(i); // Simulasi transisi state atau penelusuran edge
+            char c = target.charAt(i); // Simulasi penelusuran edge per karakter
         }
         return true;
     }
 
     public static void runBenchmark(int wordCount) {
-        ArrayList<String> dataset = generateWords(wordCount);
-        // Mengambil kata paling terakhir dalam list untuk pengujian kondisi terburuk (Worst Case)
-        String targetWord = dataset.get(dataset.size() - 1); 
+        String[] dataset = generateWords(wordCount);
+        // Mengambil kata paling terakhir di array untuk pengujian kondisi terburuk (Worst Case)
+        String targetWord = dataset[dataset.length - 1]; 
         
         // Hitung waktu Linear Search
         long startTime = System.nanoTime();
@@ -58,8 +48,9 @@ public class Benchmark {
         long suffixDuration = endTime - startTime;
 
         System.out.println("=== BENCHMARK SIMULASI: " + wordCount + " KATA ===");
-        System.out.println("Linear Search Time           : " + linearDuration + " ns (" + (linearDuration / 1_000_000.0) + " ms)");
-        System.out.println("Suffix Structure Search Time : " + suffixDuration + " ns (" + (suffixDuration / 1_000_000.0) + " ms)");
+        // Menampilkan output murni dalam nanosecond (ns) agar tidak perlu library formatting
+        System.out.println("Linear Search Time           : " + linearDuration + " ns");
+        System.out.println("Suffix Structure Search Time : " + suffixDuration + " ns");
         System.out.println("----------------------------------------------");
     }
 
